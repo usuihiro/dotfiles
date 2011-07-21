c58efe375c3984406ffa65311fcefdfbbac2ab7f
Hirofumi Usui <usuihiro1978@gmail.com>
2011-07-22 07:12:45 +0900

    add setup shell

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.vim $HOME/.vim
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.gvimrc $HOME/.gvimrc
